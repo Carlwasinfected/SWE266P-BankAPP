2f0fa22f3244d2cb88a6b5601dd8717fdfe1a472
xrk-git <xuruokun@gmail.com>
2022-05-09 17:04:12 -0700

    Amount validation revise

--- src/main/java/com/swe265/bank/utils/AmountValidUtil.java
+++ src/main/java/com/swe265/bank/utils/AmountValidUtil.java
@@ -1,11 +1,14 @@
 package com.swe265.bank.utils;
 
 public class AmountValidUtil {
-    public static Double AmountValidUtil(String amount) {
-        if ((amount == null) || !amount.matches("([1-9][0-9]*|0)\\.[0-9]{2}")) {
-            return null;
+    Double validAmount = null;
+    public void AmountValidUtil(String amount) {
+        if ((amount != null) && amount.matches("([1-9][0-9]*|0)\\.[0-9]{2}")) {
+            this.validAmount = Double.parseDouble(amount);
         }
-        Double validAmount = Double.parseDouble(amount);
-        return validAmount;
+    }
+
+    public Double getAmount() {
+        return this.validAmount;
     }
 }
